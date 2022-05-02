9259f445e5564ad1b6db56c6ad1605782c3b4a55
sufaila21 <sabbasali@millenniumci.com>
2022-05-02 21:38:19 +0530

    initial commit

--- controllers/mesh.js
+++ controllers/mesh.js
@@ -1,16 +1,19 @@
 const { exec } = require("child_process");
 
 exports.read = (req, res) => {
-  exec("node ../node_modules/meshcentral/meshctrl serverinfo  --loginuser Jaas21 --loginpass Jaas@Mesh", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return res.json(error.message);
+  exec(
+    "node ../node_modules/meshcentral/meshctrl serverinfo  --loginuser Jaas21 --loginpass Jaas@Mesh",
+    (error, stdout, stderr) => {
+      if (error) {
+        var result = { error: error.message };
+        return res.json(result);
+      }
+      if (stderr) {
+        var result = { error: stderr };
+        return res.json(result);
+      }
+      var result = { data: stdout };
+      return res.json(result);
     }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return res.json(stderr);
-    }
-    console.log(`stdout: ${stdout}`);
-    return res.json(stdout)
-  });
+  );
 };
