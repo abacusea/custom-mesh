213fc186cbf0a204a350eabe53cd067900507a80
sufaila21 <sabbasali@millenniumci.com>
2022-05-09 06:53:31 +0530

    add liistusers/userinfo

--- controllers/mesh.js
+++ controllers/mesh.js
@@ -18,24 +18,6 @@ exports.serverInfo = (req, res) => {
   );
 };
 
-exports.serverInfo = (req, res) => {
-  exec(
-    "node ../node_modules/meshcentral/meshctrl serverinfo  --loginuser Jaas21 --loginpass Jaas@Mesh",
-    (error, stdout, stderr) => {
-      if (error) {
-        var result = { error: error.message };
-        return res.json(result);
-      }
-      if (stderr) {
-        var result = { error: stderr };
-        return res.json(result);
-      }
-      var result = { 'data': stdout.split('\n')};
-      return res.json(result);
-    }
-  );
-};
-
 exports.userInfo = (req, res) => {
   exec(
     "node ../node_modules/meshcentral/meshctrl userinfo  --loginuser Jaas21 --loginpass Jaas@Mesh",
--- routes/mesh.js
+++ routes/mesh.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const {
   serverInfo,
+  userInfo,
+  listUsers
 } = require("../controllers/mesh");
 
 router.get("/mesh/serverInfo", serverInfo);
